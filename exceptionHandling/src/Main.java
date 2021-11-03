c5aa7801869483957c9bf1df24673188939fbc77
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2021-11-03 22:21:56 +0300

    Update Main.java

--- exceptionHandling/src/Main.java
+++ exceptionHandling/src/Main.java
@@ -8,9 +8,15 @@ public class Main {
                 System.out.println(sayilar[5]);
         }
         //çalışmazsa yukaridaki hata catch (Exception exception)'a gider
+            //ArrayIndexOutOfBoundsException
         catch (Exception exception) {
                 System.out.println("hata");
         }
+        //2'den fazla catch blogu olabilir.
+        //başka catch blogu içerisinde hatalar ayrıştırılabilir ve
+        //onun haricinde oluşabilecek hatalar loglanabilir.
+        //(Exception exception) böyle loglanır, diger hatalar;
+        ////ArrayIndexOutOfBoundsException bu şekilde oluşturulur
         finally { //ister try, ister catch çalışsın; finally blogu her türlü çalışacaktir.
                 System.out.println("Ben hep çalişirim");
         }
