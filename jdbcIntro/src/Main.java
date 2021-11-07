ea1a1b6f979753ff869067be33dabb8e6a97e30e
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2021-11-07 20:36:06 +0300

    insert into db

--- jdbcIntro/src/Main.java
+++ jdbcIntro/src/Main.java
@@ -1,8 +1,36 @@
 import java.sql.*;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
+        Connection connection = null;
+        DbHelper dbHelper = new DbHelper();
+
+        PreparedStatement statement = null; //sql sorgularinin işlemlerini yapacak yer
+        ResultSet resultSet;  //sorgularin sonucunda gelecek data resultset'tir
+
+        try {
+            connection = dbHelper.getConnection();
+            //System.out.println("connection established");
+            String sql = "insert into city (Name,CountryCode,District,Population) values (?,?,?,?)";
+            statement = connection.prepareStatement(sql);
+            statement.setString(1, "Nilufer");
+            statement.setString(2, "TUR");
+            statement.setString(3, "Bursa, Marmara");
+            statement.setInt(4, 570542);
+            statement.executeUpdate();
+                System.out.println("Added.");
+        }
+        catch (SQLException exception) {
+            dbHelper.showErrorMessage(exception);
+        }
+        finally {
+            statement.close();
+            connection.close();
+        }
+    }
+    public static void selectDemo() throws SQLException {
         Connection connection = null;
         DbHelper dbHelper = new DbHelper();
 
@@ -14,8 +42,11 @@ public class Main {
             //System.out.println("connection established");
             statement = connection.createStatement(); //farklı db'lerle çalisilan projede karismaması için buradaki 'connection.createStatement()' diyoruz.
             resultSet = statement.executeQuery("select Code,Name,Continent,Region from country");
+            ArrayList<Country> countries = new ArrayList<Country>();
             while (resultSet.next()) { //tek tek gezecek datayi
-                System.out.println(resultSet.getString("Name"));
+                countries.add(new Country(resultSet.getString("Code"), resultSet.getString("Name"), resultSet.getString("Continent"), resultSet.getString("Region")));
+                //System.out.println(countries);
+                System.out.println(resultSet.getString("name"));
             }
         }
         catch (SQLException exception) {
