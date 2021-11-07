a6c1d06023fc060efe77277615e67f9354fe4f00
Ataberk <67511186+ataberkcetinkaya@users.noreply.github.com>
2021-11-07 03:14:29 +0300

    Add files via upload

--- jdbcIntro/src/Main.java
+++ jdbcIntro/src/Main.java
@@ -1,15 +1,22 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         Connection connection = null;
         DbHelper dbHelper = new DbHelper();
+
+        Statement statement = null; //sql sorgularinin işlemlerini yapacak yer
+        ResultSet resultSet;  //sorgularin sonucunda gelecek data resultset'tir
+
         try {
             connection = dbHelper.getConnection();
-            System.out.println("connection established");
+            //System.out.println("connection established");
+            statement = connection.createStatement(); //farklı db'lerle çalisilan projede karismaması için buradaki 'connection.createStatement()' diyoruz.
+            resultSet = statement.executeQuery("select Code,Name,Continent,Region from country");
+            while (resultSet.next()) { //tek tek gezecek datayi
+                System.out.println(resultSet.getString("Name"));
+            }
         }
         catch (SQLException exception) {
             dbHelper.showErrorMessage(exception);
